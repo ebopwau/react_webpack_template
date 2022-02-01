d3551c1ee4a3e33fa56e2cd817cffedc1b76f123
Nick Dronov <nickdronov@MacBook-Pro-Nick.local>
2022-02-01 12:14:55 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.scss'
+
+export function App () {
+    return (
+        <div>
+            <h1>Welcome {new Date().toDateString()}</h1>
+        </div>
+    )
+}
